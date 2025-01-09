59d4f7815e55460b93a67e2087539ad690826310
Omkarbarad <164455064+Omkarbarad@users.noreply.github.com>
2025-01-09 17:49:44 +0530

    Update LikedMusic.jsx

--- frontend/src/components/LikedMusic.jsx
+++ frontend/src/components/LikedMusic.jsx
@@ -3,46 +3,46 @@ import Card from "./Card";
 import { MusicContext } from "../Context";
 
 function LikedMusic() {
-  const musicContext = useContext(MusicContext);
-  const likedMusic = musicContext.likedMusic;
-  const setlikedMusic = musicContext.setLikedMusic;
+  const { likedMusic, setLikedMusic } = useContext(MusicContext);
 
   useEffect(() => {
+    // Scroll to top when component mounts
     window.scrollTo(0, 0);
-    const localLikedMusic = JSON.parse(localStorage.getItem("likedMusic"));
-    setlikedMusic(localLikedMusic);
-  }, [setlikedMusic]);
+
+    // Retrieve liked music from localStorage
+    const localLikedMusic = JSON.parse(localStorage.getItem("likedMusic")) || [];
+    setLikedMusic(localLikedMusic);
+  }, [setLikedMusic]);
 
   return (
-    <div>
+    <div className="liked-music-container">
+      {/* If no liked music, show a friendly message */}
       {likedMusic.length === 0 ? (
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <h3 className="py-5 text-center">
-                You don't have any liked music yet!
-              </h3>
-              <div className="text-center">
-                <i className="bi bi-emoji-frown fs-1"></i>{" "}
-              </div>
-            </div>
+        <div className="container text-center">
+          <h3 className="py-5">You don't have any liked music yet!</h3>
+          <div>
+            <i className="bi bi-emoji-frown fs-1"></i>
           </div>
         </div>
       ) : (
-        <div>
+        <>
+          {/* Title for liked music */}
           <h1 className="text-danger text-center py-3">
             Your Liked Music <i className="bi bi-heart-fill text-danger"></i>
           </h1>
-        </div>
-      )}
 
-      <div className="container">
-        <div className="row">
-          {likedMusic.map((element) => {
-            return <Card key={element.id} element={element} />;
-          })}
-        </div>
-      </div>
+          {/* Display liked music cards */}
+          <div className="container">
+            <div className="row">
+              {likedMusic.map((element) => (
+                <div className="col-md-4 mb-4" key={element.id}>
+                  <Card element={element} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
